108db15f09a96c0fb101725b594a7d0409dabcca
Aoife <c00218374@itcarlow.ie>
2019-09-25 12:41:54 +0100

    Unity Testing

---
+++ RWM-P0-C00218374/Assets/Scripts/FizzBuzzFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class FizzBuzzFilter
+{
+    public static int[] fizzBuzzFunc(int[] t_array)
+    {
+        for (int i = 0; i < t_array.Length; i++)
+        {
+            if (t_array[i] % 3 == 0 && t_array[i] % 5 == 0)
+            {
+                t_array[i] = 0;
+            }
+            else if (t_array[i] % 3 == 0)
+            {
+                t_array[i] = 3;
+            }
+            else if (t_array[i] % 5 == 0)
+            {
+                t_array[i] = 5;
+            }
+        }
+
+        return t_array;
+    }
+}
---
+++ RWM-P0-C00218374/Assets/Tests/FizzBuzzTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class FizzBuzzTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void FizzBuzzTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            int[] input = { 4, 3, 6, 30, 10, 12};
+            int[] output = FizzBuzzFilter.fizzBuzzFunc(input);
+            int[] expected = { 4, 3, 3, 0, 5, 3 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator FizzBuzzTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
